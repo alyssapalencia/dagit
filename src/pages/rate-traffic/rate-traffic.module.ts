ea90e2a977539e53af7386d723db6bfbde2820e8
Alyssa Palencia <alyssalpalencia@su.edu.ph>
2017-10-19 18:24:05 +0800

    updated

---
+++ src/pages/rate-traffic/rate-traffic.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { RateTrafficPage } from './rate-traffic';
+
+@NgModule({
+  declarations: [
+    RateTrafficPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RateTrafficPage),
+  ],
+})
+export class RateTrafficPageModule {}
---
+++ src/pages/rate-traffic/rate-traffic.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the RateTrafficPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-rate-traffic',
+  templateUrl: 'rate-traffic.html',
+})
+export class RateTrafficPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RateTrafficPage');
+  }
+
+}
