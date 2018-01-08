1388f2e6ff6477491bbeb44c7c480992105aedc1
Alyssa Palencia <alyssalpalencia@su.edu.ph>
2018-01-08 12:58:08 +0800

    updated

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -16,7 +16,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class LoginPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    //this.navCtrl.setRoot(LoginPage);
+    
   }
 
   ionViewDidLoad() {
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular';
 
 
 @IonicPage()
@@ -10,10 +11,36 @@ export class TabsPage {
 
   tab1Root = 'RateTrafficPage';
   tab2Root = 'ParkingPage';
-  tab3Root = 'LogoutPage';
 
 
-  constructor() {
+  constructor(public actionSheetCtrl: ActionSheetController) {
 
+  }
+
+   presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Modify your album',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        },{
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 }
